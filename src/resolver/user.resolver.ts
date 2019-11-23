4412d031267412910855b5a6022e728a3ea26df7
Stiven Rodriguez <judrobinx@gmail.com>
2019-11-23 15:02:02 -0500

    feat: create resolver for user

---
+++ src/resolver/user.resolver.ts
@@ -0,0 +1,21 @@
+import { getRepository } from "typeorm"
+import { User } from "../entities/User"
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || 'World'}`,
+    // this is the user resolver
+    user: (_, { id }) => {
+      return getRepository(User).findOne(id)
+    },
+  },
+  Mutation: {
+    // this is the addUser resolver
+    addUser: (_, { name, email }) => {
+      const user = new User()
+      user.email = email
+      user.name = name
+      return getRepository(User).save(user)
+    },
+  },
+}
